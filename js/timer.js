a1f20094462656af53f71f60d00a6e9365b2e1b5
IrKir <75164587+IrKir@users.noreply.github.com>
2022-03-21 14:28:07 +0500

    Add files via upload

--- js/timer.js
+++ js/timer.js
@@ -8,39 +8,39 @@ const updateClock = () => {
   const date = new Date().getTime();
   const dateDeadline = new Date(deadline).getTime();
   const timeRemaining = (dateDeadline - date) / 1000; // переводим миллисекунды в секунды
-  const days = Math.floor(timeRemaining /  60 / 60 / 24); // переводим секунды в дни  
+  const days = Math.floor(timeRemaining / 60 / 60 / 24); // переводим секунды в дни  
   const hours = Math.floor((timeRemaining / 60 / 60) % 24); // переводим секунды в часы
   const minutes = Math.floor((timeRemaining / 60) % 60) // переводим часы в минуты, выносим остаток деления на 60
   const seconds = Math.floor(timeRemaining % 60);
   
-  const formatDays = days < 10 ? '0' + days : days;
+  /*const formatDays = days < 10 ? '0' + days : days;
   let formatHours //= hours < 10 ? '0' + hours : hours; // тернарный оператор
   const formatMinutes = minutes < 10 ? '0' + minutes : minutes;
-  const formatSeconds = seconds < 10 ? '0' + seconds : seconds; 
+  const formatSeconds = seconds < 10 ? '0' + seconds : seconds; */
 
-  /* Красивое
   //Добавляем нули, если меньше 10
   const formatTime = time => (time < 10 ? `0${time}` : time);
-  //Всталяем значения
-  timeField.textContent = `${formatTime(days)}:${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
-  */
+  const numWord = [' день ', ' дня ', ' дней '];  
+
+  const count = function (value, words) {
+    if (value <= 1) {
+      return words[0]
+    } else if (value > 1 && value < 5) {
+      return words[1];
+    } else if (value > 4 && value < 11) {
+      return words[2];
+    }
+  }
   
-  if (hours < 10) {
-    formatHours = '0' + hours
-  } else if (hours >= 24) {
-    formatHours = '00';
-  } else {
-    formatHours = hours;
-  }
+    //Вставляем значения
+    timerBlock.textContent = `${formatTime(days)}${count(formatTime(days), numWord)}${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
 
-  timerBlock.textContent = `${formatDays}:${formatHours}:${formatMinutes}:${formatSeconds}`;
-
-  if (timeRemaining <= 0) {
-    clearInterval(interval);
-    timerBlock.textContent = '00:00:00:00'
-  }
-};
-
+    if (timeRemaining <= 0) {
+      clearInterval(interval);
+      timerBlock.textContent = '00:00:00:00'
+    }
+  };
+  
 interval = setInterval(updateClock, 500);
 
 /* Текущее время компьютера
