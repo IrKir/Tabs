ef28368e14b85663a2cefb0e759ab0fec941f08e
IrKir <75164587+IrKir@users.noreply.github.com>
2022-03-21 17:57:03 +0500

    Add files via upload

--- js/tabs.js
+++ js/tabs.js
@@ -1,7 +1,8 @@
 const tabButtons = document.querySelectorAll('.design-list__item');
 const tabDescriptions = document.querySelectorAll('.design__descr');
 const tabImages = document.querySelectorAll('.design-images');
-const tabTitles = document.querySelectorAll('.design__title')
+const tabTitles = document.querySelectorAll('.design__title');
+const title = document.querySelector('title');
 
 const changeContent = (array, value) => {
   array.forEach((elem) => {
@@ -19,14 +20,16 @@ tabButtons.forEach((tabButton) => {
 
     changeContent(tabDescriptions, dataValue);
     changeContent(tabImages, dataValue);
-    changeContent(tabTitles, dataValue);
+    changeContent(tabTitles, dataValue);  
 
     tabButtons.forEach((btn) => {      
       if (btn === event.target) {
-        btn.classList.add('design-list__item_active')
+        btn.classList.add('design-list__item_active'); 
+        title.textContent = btn.textContent
       } else {
         btn.classList.remove('design-list__item_active')
       }
     })
   })
 });
+
